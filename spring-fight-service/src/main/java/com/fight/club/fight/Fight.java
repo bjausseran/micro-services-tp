273d2e563354c8d5705017ab1812536eca5e6a24
bjausseran <benoit.jausseran@ynov.com>
2020-11-10 17:09:57 +0100

    Arena & Fight implemented

--- spring-fight-service/src/main/java/com/fight/club/fight/Fight.java
+++ spring-fight-service/src/main/java/com/fight/club/fight/Fight.java
@@ -13,6 +13,47 @@ import com.fight.club.base.BaseEntity;
 
 @Entity
 @Table(name = "fights")
-public class Fight {
+public class Fight extends BaseEntity {
+
+	@Column(name = "fight_date")
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private LocalDate date;
+
+	@NotEmpty
+	@Column(name = "fighterA_id")
+	private Integer fighterAId;
+
+	@NotEmpty
+	@Column(name = "fighterB_id")
+	private Integer fighterBId;
+	/**
+	 * Creates a new instance of Fight for the current date
+	 */
+	public Fight() {
+		this.date = LocalDate.now();
+	}
+
+	public LocalDate getDate() {
+		return this.date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public Integer getFightAId() {
+		return this.fighterAId;
+	}
+
+	public void setFighterAId(Integer petId) {
+		this.fighterAId = petId;
+	}
+	public Integer getFighterBId() {
+		return this.fighterBId;
+	}
+
+	public void setFighterBId(Integer petId) {
+		this.fighterBId = petId;
+	}
 
 }
---
+++ spring-fight-service/src/main/java/com/fight/club/fight/FightRepository.java
@@ -0,0 +1,11 @@
+package com.fight.club.fight;
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface FightRepository extends CrudRepository<Fight, Integer> {
+
+	List<Fight> findByFighterId(Integer petId);
+
+
+}
